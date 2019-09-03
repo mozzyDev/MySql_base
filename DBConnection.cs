21045716f909856849b78193c4ae67efa7b235f3
tomaszmozgwa <tomaszmozgwa@gmail.com>
2019-09-03 18:30:17 +0200

    DBGet method

--- DBConnection.cs
+++ DBConnection.cs
@@ -10,10 +10,46 @@ namespace MySql_base
 {
     class DBConnection : IDisposable
     {
+        #region --VARIABLES--
         private static string connectionString = ConfigurationManager.AppSettings["ConnectionString"];
         private MySqlConnection connection = new MySqlConnection(connectionString);
         protected String Query { get; set; }
         protected MySqlCommand sql_cmd;
+        protected MySqlDataReader sql_reader;
+        #endregion
+
+        #region --METHODS--
+        public List<String> DBDataGet(String table, String column, int columnNr)
+        {
+            List<String> returnList = new List<String>();
+            try
+            {
+                connection.Open();
+                Query = "SELECT DISTINCT " + column + " FROM " + table;
+                sql_cmd = connection.CreateCommand();
+                sql_cmd.CommandText = Query;
+                sql_cmd.ExecuteNonQuery();
+                sql_reader = sql_cmd.ExecuteReader();
+                while (sql_reader.Read())
+                {
+                    string item = sql_reader.GetString(columnNr);
+                    returnList.Add(item);
+                }
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            returnList.Sort();
+            return returnList;
+        }
+
+        #endregion
+
 
         public void Dispose()
         {
