33fb821e5470af23d39c6c5de929f3cd89574e0e
tomaszmozgwa <tomaszmozgwa@gmail.com>
2019-09-03 18:21:39 +0200

    DBConnection class

---
+++ DBConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace MySql_base
+{
+    class DBConnection : IDisposable
+    {
+        private static string connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+        private MySqlConnection connection = new MySqlConnection(connectionString);
+        protected String Query { get; set; }
+        protected MySqlCommand sql_cmd;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
