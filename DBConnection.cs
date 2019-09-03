ecc70ad9bcb72c05d628593e5d0c48e61f3342ed
tomaszmozgwa <tomaszmozgwa@gmail.com>
2019-09-03 19:03:19 +0200

    Wykonywanie zapytań na danych

--- DBConnection.cs
+++ DBConnection.cs
@@ -19,6 +19,8 @@ namespace MySql_base
         #endregion
 
         #region --METHODS--
+        //pobieranie danych z bazy, dla wielu danych
+        //zwraca listę wyników string
         public List<String> DBDataGet(String table, String column, int columnNr)
         {
             List<String> returnList = new List<String>();
@@ -48,6 +50,53 @@ namespace MySql_base
             return returnList;
         }
 
+        //wykonywanie zapytań na danych w bazie danych
+        public void DatabaseDataChange(String Query)
+        {
+            try
+            {
+                connection.Open();
+                sql_cmd = connection.CreateCommand();
+                sql_cmd.CommandText = Query;
+                sql_cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+
+        //wykonywanie zapytań na danych w bazie danych dla jednego zwracanego elementu
+        public String DatabaseDataGetOne(String Query)
+        {
+            String item = "";
+            try
+            {
+                connection.Open();
+                sql_cmd = connection.CreateCommand();
+                sql_cmd.CommandText = Query;
+                sql_cmd.ExecuteNonQuery();
+                sql_reader = sql_cmd.ExecuteReader();
+                while (sql_reader.Read())
+                {
+                    item = sql_reader.GetString(0);
+                }
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return item;
+        }
         #endregion
 
 
